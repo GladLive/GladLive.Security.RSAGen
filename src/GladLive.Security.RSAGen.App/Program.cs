ae330da45ec71c453c17818e771caa6b8e9a59e1
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-19 03:06:11 -0500

    Setup VS Sln

---
+++ src/GladLive.Security.RSAGen.App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladLive.Security.RSAGen.App
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			//Grab the incoming args.
+			//Should contain cert name as first arg
+			ArgParser appArgs = new ArgParser(args);
+
+			//TODO: Import GladLive.Security.Common when 
+		}
+	}
+}
