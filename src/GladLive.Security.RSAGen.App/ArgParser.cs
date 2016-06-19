513a52ba1d8dbacf240618ee459aec31593708d5
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-19 02:16:42 -0500

    Added simple ArgParser

---
+++ src/GladLive.Security.RSAGen.App/ArgParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladLive.Security.RSAGen.App
+{
+	/// <summary>
+	/// Parses the incoming application arguments into a strong-typed form.
+	/// </summary>
+	public class ArgParser
+	{
+		/// <summary>
+		/// Name of the incoming certificate.
+		/// </summary>
+		public string CertName { get; }
+
+		public ArgParser(string[] args)
+		{
+			if (args == null)
+				throw new ArgumentNullException(nameof(args), "Must provide valid cert name to load.");
+
+			if (args.Count() < 1)
+				throw new ArgumentException("Must provide valid cert name as first arg to application.");
+
+			CertName = args.First();
+		}
+	}
+}
